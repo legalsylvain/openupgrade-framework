1686cec4f0db72157c90533dda20b68e38502f14
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-11-02 22:14:34 +0100

    fixup! fixup! [INIT] upgrade-pocalypse

--- openupgrade_framework/odoo_patch/__init__.py
+++ openupgrade_framework/odoo_patch/__init__.py
@@ -1,10 +1,2 @@
-from . import modules
-from . import tools
-from . import service
-
-# Nothing todo the function, the function check_security didn't changed
-from . import http
-
-# adapted to V14
-# TODO, OpenUpgrade maintainers : check if it's OK
-from . import models
+from . import odoo
+from . import addons
